4de21ac0d26d1a8be27744ab8c2a4641a2e53182
Theeranai Nakpradit <9nongarm@gmail.com>
2020-08-16 05:29:48 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	int num1,num2,sum;
+	printf("Enter number 1 > ");
+	scanf("%d", &num1);
+	printf("Enter number 2 > ");
+	scanf("%d", &num2);
+	sum = num1 + num2;
+	printf("SUM = %d\n",sum);
+return 0;
+}
